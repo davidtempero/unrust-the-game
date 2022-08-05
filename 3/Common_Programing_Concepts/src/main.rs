1387bdd6491cc05c31b14cbafaa6bd1fcd3ef092
David Tempero <davidtempero@gmail.com>
2022-08-06 00:50:03 +0100

    Read chapter 3

--- 3/Common_Programing_Concepts/src/main.rs
+++ 3/Common_Programing_Concepts/src/main.rs
@@ -1,3 +1,24 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+
+    let mut temp = String::new();
+    println!("Insert input:");
+    io::stdin().read_line(&mut temp).expect("Failed to get input");
+
+    let uint_var: u32;
+    uint_var = temp.trim().parse().expect("Failed to convert to unsigned integer");
+
+    println!("Var is {}",uint_var);
+
+    const TRIES: u8 = 8;
+
+    'first_for: for number in 1..TRIES {
+        println!("This is my {} try",number);
+    }
+    /*
+    Multiple
+    comment
+    lines
+    */
 }
